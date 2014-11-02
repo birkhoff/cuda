51ca97891c39981141bcca586e38fc66f9dd79eb
birkhoff <papstVII@gmx.de>
2014-11-02 22:30:02 +0100

    First exercise sheet solved

--- B01/src/B01.cu
+++ B01/src/B01.cu
@@ -9,6 +9,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 static const int WORK_SIZE = 256;
 
@@ -43,8 +44,10 @@ __global__ void bitreverse(void *data) {
 
 __global__ void add(int *a, int *b, int *c)
 {
+	//c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
 
-	c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+	c[index] = a[index] + b[index];
 }
 
 
@@ -52,12 +55,15 @@ __global__ void add(int *a, int *b, int *c)
  * Host function that prepares data array and passes it to the CUDA kernel.
  */
 
-#define N 1024
+#define THREADS_PER_BLOCK 1024
 
 int main(int argc, char *argv[])
 {
 
+	struct timeval t0;
+	struct timeval t1;
 
+	int N = atoi(argv[1]);
 	int *a, *b, *c; 				// host copies of a, b, c
 	int *d_a, *d_b, *d_c; 			// device copies of a, b, c
 	int size = N * sizeof(int);
@@ -81,24 +87,46 @@ int main(int argc, char *argv[])
 		b[i] = i;//rand() % 100;
 	}
 
+	gettimeofday(&t0,0);
+
 	// Copy inputs to device
 	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
 	// Launch add() kernel on GPU with N blocks
-	add<<<N,1>>>(d_a, d_b, d_c);
+	add<<<N/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_a, d_b, d_c);
 
 	// Copy result back to host
 	cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+	gettimeofday(&t1,0);
+	double time_spent = (t1.tv_sec-t0.tv_sec) + (double)(t1.tv_usec-t0.tv_usec)/1000000;
 
 	for(i = 0; i < N; i++)
 	{
 		printf("%d\n",c[i]);
 	}
 
+	printf("\nTime Spent GPU:  %f\n\n",time_spent);
 
 	// Cleanup
 	cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+
+
+	// CPU Part
+
+	gettimeofday(&t0,0);
+
+	for(i = 0; i < N; i++)
+	{
+		c[i] = a[i]+b[i];
+	}
+
+
+	gettimeofday(&t1,0);
+	time_spent = (t1.tv_sec-t0.tv_sec) + (double)(t1.tv_usec-t0.tv_usec)/1000000;
+
+	printf("\nTime Spent CPU:  %f\n\n",time_spent);
 
 	return 0;
 }
